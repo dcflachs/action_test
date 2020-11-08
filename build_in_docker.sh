3da3af6de8812faf0d00690ce945e7cc7b740ebe
dcflachs <dcflachs@users.noreply.github.com>
2020-11-08 13:49:30 -0500

    Update build_in_docker.sh

--- build_in_docker.sh
+++ build_in_docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-exec docker run --rm --tmpfs /tmp -v $PWD/archive:/mnt/output:rw -e OUTPUT_FOLDER="/mnt/output" -e KATA_VERSION -v $PWD/source:/mnt/source:ro vbatts/slackware-base:latest /mnt/source/pkg_build.sh
+exec docker run --rm --tmpfs /tmp -v $PWD/archive:/mnt/output:rw -e OUTPUT_FOLDER="/mnt/output" -e KATA_VERSION -v $PWD/source:/mnt/source:ro vbatts/slackware:latest /mnt/source/pkg_build.sh
